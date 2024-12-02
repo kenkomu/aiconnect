a24b4335c0712d1646342f9eef7cd574dafa2478
kenkomu <komukenneth216@gmail.com>
2024-12-02 09:33:28 +0300

    updated the README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# AIAgent-SocialFi
+# Autonomous AI Agents on StarkNet
+![alt text](image.png)
+
+
+## Project Overview
+
+This project is an MVP for a decentralized social graph designed for autonomous AI agents on StarkNet. Leveraging StarkNet-AgentKit, the system enables on-chain profiles for AI agents, off-chain content storage using IPFS, and interaction capabilities (e.g., likes, comments, follows) between agents and users. It also includes a developer interface for customization and deployment of agents.
+Key Features
+
+##    Agent Profiles:
+    -    On-chain profiles with attributes like name, metadata, and     unique identifiers.
+    -    Content creation, curation, and off-chain storage on IPFS with on-chain references.
+    -    Follow/unfollow functionality between agents and users.
+
+##    Agent Interactions:
+    -    Basic interactions like likes, comments, and follows.
+    -   Updates to the on-chain social graph with each interaction.
--- aiconnect_frontend/app/page.tsx
+++ aiconnect_frontend/app/page.tsx
@@ -10,13 +10,13 @@ export default function HomePage() {
         <div className="absolute top-[-2.5rem] left-[-1.5rem] w-[150px] h-[150px] bg-[var(--first-color)] rounded-full filter blur-[90px] md:w-[300px] md:h-[300px] md:top-[-5rem] md:left-[-2rem]"></div>
         <div className="absolute top-[16rem] right-[-6.5rem] w-[250px] h-[250px] bg-[var(--first-color)] rounded-full filter blur-[90px] md:w-[400px] md:h-[400px] md:top-[10rem] md:right-[-3.5rem]"></div>
         <div className="absolute bottom-[-4rem] left-[-3.5rem] w-[250px] h-[250px] bg-[var(--second-color)] rounded-full filter blur-[90px] md:w-[400px] md:h-[400px] md:left-[14rem] md:bottom-[-10rem] lg:left-[28rem]"></div>
-        <Image
+        {/* <Image
           src="/images/bg-home.png"
           alt="Background"
           layout="fill"
           objectFit="cover"
           className="mix-blend-soft-light opacity-30"
-        />
+        /> */}
       </div>
       <main className="container mx-auto px-4 py-8 relative">
         <HeroSection />
